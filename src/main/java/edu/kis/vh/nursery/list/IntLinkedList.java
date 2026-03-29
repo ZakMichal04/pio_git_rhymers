15398dfe8d3e2aa3020d19032d88d05470cc60b3
Michał Żak <255743@edu.p.lodz.pl>
2026-03-29 15:57:52 +0200

    11. Hemetyzacja w plikach IntLinkedList i Node, zamiana stałej -1 na EMPTY_STACK

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,7 +1,7 @@
 package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
-
+    private static final int EMPTY_STACK = -1;
     private Node last;
     private int i;
 
@@ -9,9 +9,9 @@ public class IntLinkedList {
         if (last == null)
             last = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
         }
     }
 
@@ -25,15 +25,15 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return -1;
-        return last.value;
+            return EMPTY_STACK;
+        return last.getValue();
     }
 
     public int pop() {
         if (isEmpty())
             return -1;
-        int ret = last.value;
-        last = last.prev;
+        int ret = last.getValue();
+        last = last.getPrev();
         return ret;
     }
 
--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,6 +2,27 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
+    public int getValue(){
+        return value;
+    }
+    public void setValue(int value){
+        this.value = value;
+    }
+    public void setPrev(Node prev){
+        this.prev = prev;
+    }
+    public void setNext(Node next){
+        this.next = next;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
     public int value;
     public Node prev, next;
 
