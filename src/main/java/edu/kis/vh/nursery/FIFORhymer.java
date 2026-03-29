dcfd7794cb3aa222ed29d0cb282e862268223b8a
macfir <mfirlej2004@gmail.com>
2026-03-29 16:54:57 +0200

    8. Dodanie adnotacji Override w HanoiRhymer i usunięcie zbędnych adnotacji

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -4,7 +4,7 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
-    @Override
+
     public int countOut() {
         while (!callCheck())
             temp.countIn(super.countOut());
